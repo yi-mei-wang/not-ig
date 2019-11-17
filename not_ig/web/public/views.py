a5ce125418609a3b0a25a1262a24d26e41031cf0
mei <yimei.wym@gmail.com>
2019-11-17 16:23:08 +0800

    add catch all route

--- not_ig/web/public/views.py
+++ not_ig/web/public/views.py
@@ -7,3 +7,9 @@ public_blueprint = Blueprint('public', __name__)
 @public_blueprint.route('/', methods=['GET'])
 def index():
     return render_template("public/home.html")
+
+
+# Catch all routes
+@public_blueprint.route('/', methods=["GET"])
+def catch_all(*args, **kwargs):
+    return render_template("frontend/public/index.html")
