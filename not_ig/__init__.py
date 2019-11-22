1a64e571670e4d6f7af0dbf73dc49bfbf9b13f60
mei <yimei.wym@gmail.com>
2019-11-22 17:04:10 +0800

    register users api route

--- not_ig/__init__.py
+++ not_ig/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 
 import not_ig.extensions
+from not_ig.api.blueprints.users_api import users_api_blueprint
 from not_ig.web.public.views import public_blueprint
 
 
@@ -22,7 +23,8 @@ def create_app():
 
 
 def register_blueprints(app):
-    app.register_blueprint(public_blueprint, prefix='/')
+    app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
+    app.register_blueprint(public_blueprint, url_prefix='/')
 
 
 def register_extensions(app):
