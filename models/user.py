f4da5b2c5d7b3739c8f3b37cf5c7f73945df9fd1
mei <yimei.wym@gmail.com>
2019-11-22 12:19:53 +0800

    create user table

--- models/user.py
+++ models/user.py
@@ -2,6 +2,6 @@ from not_ig.extensions import db
 
 
 class User(db.Model):
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
