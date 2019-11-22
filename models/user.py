7a77b0022236769fd80f604b15b163e783497263
mei <yimei.wym@gmail.com>
2019-11-22 12:11:10 +0800

    add user model

--- models/user.py
+++ models/user.py
@@ -1,1 +1,7 @@
-import peewee as pw
+from not_ig.extensions import db
+
+
+class User(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
