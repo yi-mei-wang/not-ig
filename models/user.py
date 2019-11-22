d9c6a57766333525e5214c93876b10c995ed6e1f
mei <yimei.wym@gmail.com>
2019-11-22 14:31:01 +0800

    add profile image field

--- models/user.py
+++ models/user.py
@@ -1,3 +1,6 @@
+from flask import current_app
+from sqlalchemy.ext.hybrid import hybrid_property
+
 from not_ig.extensions import db
 
 
@@ -5,3 +8,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    profile_image = db.Column(db.String(100), nullable=False)
+
+    @hybrid_property
+    def profile_image_url(self):
+        return f'${current_app.config.get("S3_LOCATION")}${self.profile_image}'
