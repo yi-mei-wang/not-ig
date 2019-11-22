9618349a7aebfb7f7b315943ffc3763837af552e
mei <yimei.wym@gmail.com>
2019-11-22 11:02:25 +0800

    init sqlalchemy db

--- not_ig/__init__.py
+++ not_ig/__init__.py
@@ -1,15 +1,29 @@
+import os
+
 from flask import Flask
 
+import extensions
 from not_ig.web.public.views import public_blueprint
 
 
-def create_app():
+def create_app():       
     app = Flask(__name__)
 
+    if os.getenv('FLASK_ENV') == 'production':
+        app.config.from_object("config.ProductionConfig")
+    else:                       
+        app.config.from_object("config.DevelopmentConfig")
+
+
     register_blueprints(app)
+    register_extensions(app)
 
     return app
 
 
 def register_blueprints(app):
     app.register_blueprint(public_blueprint, prefix='/')
+
+
+def register_extensions(app):
+    extensions.db.init_app(app)
