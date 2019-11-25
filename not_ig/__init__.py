06061bcfc5eff1909f2a351041bd03ebfd131d9a
mei <yimei.wym@gmail.com>
2019-11-25 10:52:58 +0800

    register images api blueprint

--- not_ig/__init__.py
+++ not_ig/__init__.py
@@ -4,17 +4,17 @@ from flask import Flask
 
 import not_ig.extensions
 from not_ig.api.blueprints.users_api import users_api_blueprint
+from not_ig.api.blueprints.images_api import images_api_blueprint
 from not_ig.web.public.views import public_blueprint
 
 
-def create_app():       
+def create_app():
     app = Flask(__name__)
 
     if os.getenv('FLASK_ENV') == 'production':
         app.config.from_object("config.ProductionConfig")
-    else:                       
+    else:
         app.config.from_object("config.DevelopmentConfig")
-
 
     register_blueprints(app)
     register_extensions(app)
@@ -23,6 +23,7 @@ def create_app():
 
 
 def register_blueprints(app):
+    app.register_blueprint(images_api_blueprint, url_prefix='/api/v1/images')
     app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
     app.register_blueprint(public_blueprint, url_prefix='/')
 
