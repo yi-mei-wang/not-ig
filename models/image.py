bb55f3f503122b3d9cad77b7bf906e67e4fc68e2
mei <yimei.wym@gmail.com>
2019-11-25 10:52:23 +0800

    add image model

---
+++ models/image.py
@@ -0,0 +1,8 @@
+from not_ig.extensions import db
+from models.user import User
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
