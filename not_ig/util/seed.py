2fef8d0c1b03fff77888e2afb71a54c9bdbe9a44
mei <yimei.wym@gmail.com>
2019-11-25 15:20:49 +0800

    seed image table

--- not_ig/util/seed.py
+++ not_ig/util/seed.py
@@ -1,6 +1,5 @@
 from werkzeug.security import generate_password_hash
 
-# from models.image import Image
 from not_ig.extensions import db
 
 
@@ -18,27 +17,50 @@ def insert_users(users):
         )
         queries.append(new_user)
 
-    db.session.add_all(queries)
-    db.session.commit()
+    return queries
+
+
+def insert_images(images):
+    from models.image import Image
+
+    queries = []
+    for index, img in enumerate(images):
+        new_img = Image(
+            path=img,
+            user_id=index+1)
+        queries.append(new_img)
+
+    return queries
 
 
 def run():
-    insert_users(
-        [
-            {
-                "id": 1,
-                "username": "blake",
-                "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/idol2-blake.jpg"
-            },
-            {
-                "id": 2,
-                "username": "ryanG",
-                "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/idol1-ryan.jpg"
-            },
-            {
-                "id": 3,
-                "username": "bigfan",
-                "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/bigfan-9AE7468E-4C35-41D1-AA68-31939891B5E1.png"
-            }
-        ]
-    )
+    user_queries = insert_users(users)
+    image_queries = insert_images(images)
+
+    db.session.add_all(user_queries + image_queries)
+    db.session.commit()
+
+
+users = [
+    {
+        "id": 1,
+        "username": "blake",
+        "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/idol2-blake.jpg"
+    },
+    {
+        "id": 2,
+        "username": "ryanG",
+        "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/idol1-ryan.jpg"
+    },
+    {
+        "id": 3,
+        "username": "bigfan",
+        "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/bigfan-9AE7468E-4C35-41D1-AA68-31939891B5E1.png"
+    }
+]
+
+
+images = [
+    "http://next-curriculum-instagram.s3.amazonaws.com/1-paris2.jpeg",
+    "http://next-curriculum-instagram.s3.amazonaws.com/1-paris3.jpeg",
+    "http://next-curriculum-instagram.s3.amazonaws.com/1-paris4.jpg"]
