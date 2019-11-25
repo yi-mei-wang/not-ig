39433e4e2d5aecead2460701b89b895473951e92
mei <yimei.wym@gmail.com>
2019-11-25 16:37:37 +0800

    fix wrong links

--- frontend/src/constants/APIUrls.js
+++ frontend/src/constants/APIUrls.js
@@ -2,7 +2,7 @@ export const APIUrls = {
   myImages: "/images/me",
   // allUsers:"/api/v1/users/",
   allUsers: "/users/",
-  userImages: "/images?userId=",
+  userImages: "/images/?userId=",
   userInfo: "/users/",
   login: "/login",
   signup: "/users/"
