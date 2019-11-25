f8bb0a2de0cb340335d869081566ff6e73fce964
mei <yimei.wym@gmail.com>
2019-11-25 16:19:28 +0800

    return all images belonging to a particular user

--- not_ig/api/blueprints/images_api.py
+++ not_ig/api/blueprints/images_api.py
@@ -1,6 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 
 from models.image import Image
 
 
 images_api_blueprint = Blueprint('images_api', __name__)
+
+
+@images_api_blueprint.route('/', methods=['GET'])
+def index():
+    user_id = request.args.get('userId')
+    images = [img.path for img in Image.query.filter_by(user_id=user_id)]
+
+    return jsonify(images)
