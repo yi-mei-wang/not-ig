f6bd17edfb96df7eba8496a8873c2908c6af2d00
mei <yimei.wym@gmail.com>
2019-11-25 15:05:23 +0800

    remove extra lines

--- app.py
+++ app.py
@@ -2,19 +2,3 @@ from not_ig import create_app
 
 app = create_app()
 app.app_context().push()
-
-
-# import os
-
-# from flask import Flask
-
-
-# web_dir = os.path.join(os.path.dirname(
-#     os.path.abspath(__file__)), 'not-ig'
-# )
-
-# app = Flask('NOT-IG', root_path=web_dir)
-
-
-# if __name__ == '__main__':
-#     app.run()
