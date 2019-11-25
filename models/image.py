45fb066177f55c73131fc8a17bb9ba73fb0544a9
mei <yimei.wym@gmail.com>
2019-11-25 16:11:56 +0800

    change image model structure

--- models/image.py
+++ models/image.py
@@ -5,4 +5,5 @@ from models.user import User
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     path = db.Column(db.String(120), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete='CASCADE'))
