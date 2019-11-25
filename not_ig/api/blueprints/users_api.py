9081da71d91e511683e4f4fd9525ff75850a0c9a
mei <yimei.wym@gmail.com>
2019-11-25 16:27:13 +0800

    return data from db

--- not_ig/api/blueprints/users_api.py
+++ not_ig/api/blueprints/users_api.py
@@ -1,25 +1,19 @@
 from flask import Blueprint, jsonify
 
+from models.user import User
+
 
 users_api_blueprint = Blueprint('users_api', __name__)
 
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
-    return jsonify([
+    all_users = [
         {
-            "id": 1,
-            "username": "blake",
-            "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/idol2-blake.jpg"
-        },
-        {
-            "id": 2,
-            "username": "ryanG",
-            "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/idol1-ryan.jpg"
-        },
-        {
-            "id": 3,
-            "username": "bigfan",
-            "profileImage": "http://next-curriculum-instagram.s3.amazonaws.com/bigfan-9AE7468E-4C35-41D1-AA68-31939891B5E1.png"
-        }
-    ])
+            "id": user.id,
+            "username": user.username,
+            "profileImage": user.profile_image
+        } for user in User.query.all()
+    ]
+
+    return jsonify(all_users)
