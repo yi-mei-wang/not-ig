b9e9923ed5e582d45be59cd10d2672240a4f5528
mei <yimei.wym@gmail.com>
2019-11-26 15:12:04 +0800

    return full url using hybrid property

--- models/image.py
+++ models/image.py
@@ -1,5 +1,8 @@
-from not_ig.extensions import db
+from flask import current_app
+from sqlalchemy.ext.hybrid import hybrid_property
+
 from models.user import User
+from not_ig.extensions import db
 
 
 class Image(db.Model):
@@ -7,3 +10,7 @@ class Image(db.Model):
     path = db.Column(db.String(120), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete='CASCADE'))
+
+    @hybrid_property
+    def img_url(self):
+        return f'{current_app.config.get("S3_LOCATION")}{self.profile_image}'
