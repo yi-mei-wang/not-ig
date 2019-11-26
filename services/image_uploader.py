bf5f5b3ed54158a01cdf2bee5b6cda47f9278515
mei <yimei.wym@gmail.com>
2019-11-26 15:07:21 +0800

    upload img to s3

--- services/image_uploader.py
+++ services/image_uploader.py
@@ -5,15 +5,20 @@ import botocore
 from flask import current_app
 
 
-class ImageUploader(img):
+class ImageUploader:
     def __init__(self, img):
         self.s3_client = None
         self.img = img
+        self.path = None
         self.errors = []
 
     def run(self):
-        self._check_file(self)
-        self._set_up_client(self)
+        self._check_file()
+        self._set_up_client()
+        self._upload_to_s3()
+        breakpoint()
+
+        return (self.path, self.errors)
 
     def _check_file(self):
         if not self.img or not self.img.filename:
@@ -25,11 +30,25 @@ class ImageUploader(img):
             self.errors.append("Invalid file type")
 
     def _set_up_client(self):
-        if self.errors:
-            raise
-
         self.s3_client = boto3.client(
             "s3",
-            aws_access_key_id=current_app.config.S3_KEY,
-            aws_secret_access_key=current_app.config.S3_SECRET
+            aws_access_key_id=current_app.config.get('S3_KEY'),
+            aws_secret_access_key=current_app.config.get('S3_SECRET')
         )
+
+    def _upload_to_s3(self, acl="public-read"):
+        try:
+            self.s3_client.upload_fileobj(
+                self.img,
+                current_app.config.get('S3_BUCKET'),
+                self.img.filename,
+                ExtraArgs={
+                    "ACL": acl,
+                    "ContentType": self.img.content_type
+                }
+            )
+
+            self.path = self.img.filename
+
+        except Exception as e:
+            self.errors.append(e)
